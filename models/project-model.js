66b941024efd094ab9385d164ae79bdb531145c3
FrederickKumah <fdblay@gmail.com>
2024-11-01 12:14:30 +0000

    populated the project model

--- models/project-model.js
+++ models/project-model.js
@@ -2,7 +2,22 @@ import { Schema } from "mongoose";
 import { toJSON } from "@reis/mongoose-to-json";
 
 export const projectSchema = new Schema({
-
+    name: { type: String, required: true },
+    scope: { type: String, required: true },
+    budget: { type: Number, required: true },
+    KeyRequirements: { type: [String], required: true },
+    status: {
+        type: String,
+        default: 'Maturation',
+        enum: [
+            'Maturation',
+            'Procurement',
+            'Execution',
+            'Monitoring'
+        ]
+    },
+    createdAt: { type: Date, default: Date.now },
+    completedAt: { type: Date },
 }, {
     timestamps: true
 });
