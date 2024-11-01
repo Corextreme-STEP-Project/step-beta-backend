45d5cdf24af1171e37d5ccb984ca8dd0e9a343d7
Felicity Semetey <felicitysemetey628@gmail.com>
2024-11-01 14:24:37 +0000

    updated project model

--- models/compliance-model.js
+++ models/compliance-model.js
@@ -2,11 +2,12 @@ import { Schema } from "mongoose";
 import { toJSON } from "@reis/mongoose-to-json";
 
 export const complianceSchema = new Schema({
-
+project
+user
 }, {
     timestamps: true
 });
 
 userSchema.plugin(toJSON);
 
-export const complianceModel = model('user', complianceSchema);
+export const complianceModel = model('compliance', complianceSchema);
--- models/project-model.js
+++ models/project-model.js
@@ -1,12 +1,22 @@
-import { Schema } from "mongoose";
+import { Schema, model, Types } from "mongoose";
 import { toJSON } from "@reis/mongoose-to-json";
 
 export const projectSchema = new Schema({
-
+    projectOwner: { type: Types.ObjectId, required: true, ref: 'user' },
+    projectTitle: { type: String, required: true },
+    description: { type: String, required: true },
+    scope: { type: String, required: true },
+    budget: { type: Number, required: true },
+    keyRequirements: { type: [String], required: true },
+    projectStatus: { type: String, default: 'Maturation', enum: ['Maturation', 'Procurement', 'Execution', 'Monitoring'] },
+    projectBegins: { type: Date, default: Date.now },
+    projectEnds: { type: Date },
+    duration: { type: String },
+    projectCompliance: { type: Boolean }
 }, {
     timestamps: true
 });
 
 projectSchema.plugin(toJSON);
 
-export const userModel = model('user', projectSchema);
+export const projectModel = model('project', projectSchema);
