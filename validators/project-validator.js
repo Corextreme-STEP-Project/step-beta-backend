8aafee095020a016dd17a5c92c0bbf4c9c562eeb
FrederickKumah <fdblay@gmail.com>
2024-11-03 14:45:31 +0000

    created the project status update controller, and routes

--- validators/project-validator.js
+++ validators/project-validator.js
@@ -5,3 +5,8 @@ export const createProjectValidator = Joi.object({
   budget: Joi.number().required(),
   keyRequirements: Joi.array().items(Joi.string()).required(),
 });
+
+export const projectStatusUpdate = Joi.object({
+  projectStatus: Joi.string().required(),
+  statusDescription: Joi.string().required()
+});
