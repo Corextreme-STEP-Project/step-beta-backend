45c64f04b0543da2da263f6b0e032c320345e103
Jacqueline Offei <offeijacqueline@gmail.com>
2024-11-04 11:38:15 +0000

    adding updates to project validator

--- validators/project-validator.js
+++ validators/project-validator.js
@@ -1,6 +1,8 @@
 import Joi from "joi";
 
 export const createProjectValidator = Joi.object({
+  projectTitle:Joi.string().required(),
+  description:Joi.string().required(),
   scope: Joi.string().required(),
   budget: Joi.number().required(),
   keyRequirements: Joi.array().items(Joi.string()).required(),
