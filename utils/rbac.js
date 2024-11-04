3f062bb4ae070f5a740181f32e1473ec4e8e6b28
Berikisu Ibnrahim <berikisuibrahim227@gmail.com>
2024-11-04 14:48:01 +0000

    created rbac.js file, and worked on the role based permissions in the Auth.js file

---
+++ utils/rbac.js
@@ -0,0 +1,25 @@
+export const permissions = [
+    {
+        role: 'user',
+        actions: [
+            'get_project',
+            'get_project_id',
+            'count_project'
+
+
+        ]
+    },
+    {
+        role: 'vendor',
+        actions: [
+            'get_project',
+            'get_project_id',
+            'post_project',
+            'update_project',
+            'delete_project',
+            'count_project'
+
+
+        ]
+    }
+]
