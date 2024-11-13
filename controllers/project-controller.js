aee009ca68970eece77ae4d128691a66f889189b
Jacqueline Offei <offeijacqueline@gmail.com>
2024-11-13 10:18:25 +0000

    adding notification scheduler for status update and project deadline

--- controllers/project-controller.js
+++ controllers/project-controller.js
@@ -1,5 +1,12 @@
 import { projectModel } from "../models/project-model.js";
-import { createProjectValidator, projectStatusUpdate } from "../validators/project-validator.js";
+import {
+  scheduleProjectDeadlineNotification,
+  scheduleStatusUpdateNotfication,
+} from "../utils/scheduler-project-deadline.js";
+import {
+  createProjectValidator,
+  projectStatusUpdate,
+} from "../validators/project-validator.js";
 
 export const createProject = async (req, res, next) => {
   try {
@@ -8,7 +15,15 @@ export const createProject = async (req, res, next) => {
       return res.status(422).json(error);
     }
     // create project using validated input
-    await projectModel.create(value);
+    const newProject = await projectModel.create(value);
+    // schedule notification for project deadline 3 days before projectEnds
+    if (newProject.projectEnds) {
+      scheduleProjectDeadlineNotification(
+        newProject.projectEnds,
+        newProject._id,
+        io
+      );
+    }
     // send success response
     res.status(201).json("Project created successfully");
   } catch (error) {
@@ -39,23 +54,18 @@ export const getProject = async (req, res, next) => {
 
 export const updateProjectStatus = async (req, res, next) => {
   try {
-    const statuses = [
-      'Maturation',
-      'Procurement',
-      'Execution',
-      'Monitoring'
-    ];
-    const { projectStatus } = req.body
+    const statuses = ["Maturation", "Procurement", "Execution", "Monitoring"];
+    const { projectStatus } = req.body;
     // validation
     const { error, value } = projectStatusUpdate.validate(req.body);
     if (error) {
       return res.status(422).json(error);
     }
-    
+
     // Find Project by Id
     const project = await projectModel.findById(req.params.id);
     if (!project) {
-      return res.status(404).json("Project not found!")
+      return res.status(404).json("Project not found!");
     }
 
     // Check if the requested status is the next logical status from the current one
@@ -63,15 +73,28 @@ export const updateProjectStatus = async (req, res, next) => {
     const newProjectStatusIndex = statuses.indexOf(projectStatus);
 
     if (newProjectStatusIndex !== currentProjectStatusIndex + 1) {
-      return res.status(400).json({ message: `Cannot move from ${project.projectStatus} to ${projectStatus}. Follow the order` })
+      return res.status(400).json({
+        message: `Cannot move from ${project.projectStatus} to ${projectStatus}. Follow the order`,
+      });
     }
 
     // Update project status
-    await projectModel.findByIdAndUpdate(req.params.id, value);
+    const updatedProject = await projectModel.findByIdAndUpdate(
+      req.params.id,
+      value
+    );
 
+    // schedule notification after project status is updated
+    if (updatedProject) {
+      scheduleStatusUpdateNotfication(
+        updatedProject._id,
+        updatedProject.projectStatus,
+        io
+      );
+    }
     // Response
     res.status(200).json("Project Status updated Successfully!");
   } catch (error) {
     next(error);
   }
-}
+};
