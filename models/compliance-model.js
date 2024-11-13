431dc715c1ce69eb76e1d7b625a7ccfb7bb25216
Felicity Semetey <felicitysemetey628@gmail.com>
2024-11-13 19:01:52 +0000

    compliance validator

--- models/compliance-model.js
+++ models/compliance-model.js
@@ -1,11 +1,11 @@
 import { Schema, model, Types } from "mongoose";
-import { toJSON } from "../step-project/node_modules/@reis/mongoose-to-json";
+import { toJSON } from "@reis/mongoose-to-json";
 
 export const complianceSchema = new Schema({
   project: { type: Types.ObjectId, ref: 'Project', required: true },
   complianceStatus: { type: String, enum: ['compliant', 'non-compliant', 'pending'], default: 'pending' },
-  notes: { type: String },
-  checkedBy: { type: Types.ObjectId, ref: 'User' },
+  notes: [{ type: String }],
+  checkedBy: [{ type: Types.ObjectId, ref: 'User' }],
 }, { timestamps: true }
 );
 
